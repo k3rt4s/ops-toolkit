9d9409043b5d51abf8bb9b52853889a3c68ecc62
Jon Bowker <35662945+k3rt4s@users.noreply.github.com>
2020-10-19 20:50:21 -0500

    Organization

---
+++ Misc/Darkmode-Disable.ps1
@@ -0,0 +1,5 @@
+Write-Host "Disabling Dark Mode"
+$Theme = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+Set-ItemProperty $Theme AppsUseLightTheme -Value 1
+Start-Sleep 1
+Write-Host "Disabled"

6b9a032026b1b9777c918ba608fc7261e1f61855
Jon Bowker <35662945+k3rt4s@users.noreply.github.com>
2021-04-13 18:31:16 -0500

    Update

--- Elk Lab/Elk.sh
+++ Elk Lab/Elk.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
-​
-if ! [ $(id -u) = 0 ]; then
-   echo "The script need to be run as root." >&2
-   exit 1
-fi
-​
-if [ $SUDO_USER ]; then
-    real_user=$SUDO_USER
-else
-    real_user=$(whoami)
-fi
-#Check to see if user is Root
+​#Check to see if user is Root
 #
-apt install docker.io 
+apt install docker.io &&
 #installs docker to box
 #
-apt install python3-pip
+apt install python3-pip &&
 #installs python
 #
-apt install docker
+apt install docker &&
 #installs docker python module
 #
-sysctl -w vm.max_map_count=262144
+sysctl -w vm.max_map_count=262144 &&
 #increases virtual memory
 #
-systemctl start docker 
-systemctl enable docker.service
-systemctl enable containerd.service
+systemctl start docker &&
+systemctl enable docker.service &&
+systemctl enable containerd.service &&
 #starts docker
 #
-docker pull sebp/elk
+docker pull sebp/elk &&
 #pulls docker elk
 #
-docker run -it -p 5601:5601 -p 9200:9200 -p 5044:5044 --restart always sebp/elk 
+docker run -it -p 5601:5601 -p 9200:9200 -p 5044:5044 --restart always sebp/elk && 
 #Create Image
-#
